bff979d3bbbfdd24d59858d185bbe6d8148b64f7
Günther Foidl <gue@korporal.at>
2017-09-22 13:47:55 +0200

    DataPointSerializer can handle DateTimes via a date format string

--- source/gfoidl.DataCompression/DataPointSerializer.cs
+++ source/gfoidl.DataCompression/DataPointSerializer.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
 using static System.FormattableString;
@@ -16,8 +17,12 @@ namespace gfoidl.DataCompression
         /// <param name="csvFile">The csv file</param>
         /// <param name="separator">The delimiter in the csv file. Defaults to TAB</param>
         /// <param name="firstLineIsHeader">When <c>true</c> the first line is skipped</param>
+        /// <param name="dateTimeFormat">
+        /// A custom <see cref="DateTime" /> format string, that is used to parse the first column,
+        /// or <c>null</c> when the first column should be parsed as <see cref="System.Double" />.
+        /// </param>
         /// <returns>The <see cref="DataPoint" />s read from the csv file</returns>
-        public IEnumerable<DataPoint> Read(string csvFile, char? separator = null, bool firstLineIsHeader = true)
+        public IEnumerable<DataPoint> Read(string csvFile, char? separator = null, bool firstLineIsHeader = true, string dateTimeFormat = null)
         {
             char sep = separator ?? '\t';
 
@@ -28,8 +33,16 @@ namespace gfoidl.DataCompression
                 while (!sr.EndOfStream)
                 {
                     string[] cols = sr.ReadLine().Split(sep);
+                    double x      = default;
 
-                    double x = double.Parse(cols[0], NumberStyles.Any, CultureInfo.InvariantCulture);
+                    if (dateTimeFormat == null)
+                        x = double.Parse(cols[0], NumberStyles.Any, CultureInfo.InvariantCulture);
+                    else
+                    {
+                        DateTime dt = DateTime.ParseExact(cols[0], dateTimeFormat, CultureInfo.InvariantCulture);
+                        x           = dt.Ticks;
+                    }
+
                     double y = double.Parse(cols[1], NumberStyles.Any, CultureInfo.InvariantCulture);
 
                     yield return (x, y);
@@ -44,7 +57,11 @@ namespace gfoidl.DataCompression
         /// <param name="data">The <see cref="DataPoint" />s to write</param>
         /// <param name="separator">The delimiter in the csv file. Defaults to TAB</param>
         /// <param name="header">The header to use, or <c>null</c> to omit any header</param>
-        public void Write(string csvFile, IEnumerable<DataPoint> data, char? separator = null, (string X, string Y)? header = null)
+        /// <param name="dateTimeFormat">
+        /// A custom <see cref="DateTime" /> format string, that is used to write the first column,
+        /// or <c>null</c> when the first column should be written as <see cref="System.Double" />.
+        /// </param>
+        public void Write(string csvFile, IEnumerable<DataPoint> data, char? separator = null, (string X, string Y)? header = null, string dateTimeFormat = null)
         {
             char sep = separator ?? '\t';
 
@@ -54,7 +71,10 @@ namespace gfoidl.DataCompression
                     sw.WriteLine($"# {header.Value.X}{sep}{header.Value.Y}");
 
                 foreach (DataPoint dp in data)
-                    sw.WriteLine(Invariant($"{dp.X}{sep}{dp.Y}"));
+                    if (dateTimeFormat == null)
+                        sw.WriteLine(Invariant($"{dp.X}{sep}{dp.Y}"));
+                    else
+                        sw.WriteLine($"{dp.ToTimeValue().Time.ToString(dateTimeFormat)}{sep}{Invariant($"{dp.Y}")}");
             }
         }
     }
