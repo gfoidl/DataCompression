bfcf0903dee5de0c55f29e7741f86d7cedf3ccc0
Günther Foidl <gue@korporal.at>
2017-12-26 14:08:33 +0100

    DataPoint as readonly struct

--- source/gfoidl.DataCompression/DataPoint.cs
+++ source/gfoidl.DataCompression/DataPoint.cs
@@ -5,17 +5,21 @@ namespace gfoidl.DataCompression
     /// <summary>
     /// A (x,y) point.
     /// </summary>
-    public struct DataPoint : IEquatable<DataPoint>
+    public readonly struct DataPoint : IEquatable<DataPoint>
     {
+        private static readonly DataPoint _origin = new DataPoint();
+        //---------------------------------------------------------------------
         /// <summary>
         /// x value
         /// </summary>
         public double X { get; }
         //---------------------------------------------------------------------
-            /// <summary>
-            /// y value
-            /// </summary>
+        /// <summary>
+        /// y value
+        /// </summary>
         public double Y { get; }
+        //---------------------------------------------------------------------
+        public static ref readonly DataPoint Origin => ref _origin;
         //---------------------------------------------------------------------
         /// <summary>
         /// Creates a new <see cref="DataPoint" />
@@ -86,12 +90,7 @@ namespace gfoidl.DataCompression
         /// </summary>
         /// <param name="obj">The object to compare with this one.</param>
         /// <returns><c>true</c> if equal, <c>false</c> otherwise</returns>
-        public override bool Equals(object obj)
-        {
-            if (obj is DataPoint other) return this.Equals(other);
-
-            return false;
-        }
+        public override bool Equals(object obj) => obj is DataPoint other && this.Equals(other);
         //---------------------------------------------------------------------
         /// <summary>
         /// Tests for equality between the given <see cref="DataPoint" />s.
@@ -135,7 +134,6 @@ namespace gfoidl.DataCompression
         /// A string that represents the current object in the form (x,y).
         /// </returns>
         public override string ToString() => $"({this.X}, {this.Y})";
-
         //---------------------------------------------------------------------
 #pragma warning disable CS1591 // Missing XML comment for publicly visible type or member
         public static implicit operator DataPoint((double, double) tuple)   => new DataPoint(tuple);
