bb5a99062604e42040bbfb0371dac941d29c509c
Günther Foidl <gue@korporal.at>
2018-02-05 14:27:06 +0100

    DataPointIterator added as base class for further iterators

--- source/gfoidl.DataCompression/ThrowHelper.cs
+++ source/gfoidl.DataCompression/ThrowHelper.cs
@@ -5,5 +5,6 @@ namespace gfoidl.DataCompression
     internal static class ThrowHelper
     {
         public static void ThrowArgumentNull(string argName) => throw new ArgumentNullException(argName);
+        public static void ThrowNotSupported()               => throw new NotSupportedException();
     }
 }
