7d3acc8b46311cfe4e182f7ee6b323fa54b6aa79
Günther Foidl <gue@korporal.at>
2018-02-05 22:46:27 +0100

    DataPointIterator as return type from ICompression.Process

--- source/gfoidl.DataCompression/Compression/NoCompression.cs
+++ source/gfoidl.DataCompression/Compression/NoCompression.cs
@@ -12,6 +12,27 @@ namespace gfoidl.DataCompression
         /// </summary>
         /// <param name="data">Input data</param>
         /// <returns>The compressed / filtered data.</returns>
-        protected override IEnumerable<DataPoint> ProcessCore(IEnumerable<DataPoint> data) => data;
+        protected override DataPointIterator ProcessCore(IEnumerable<DataPoint> data)
+            => new EnumerableIterator(data.GetEnumerator());
+        //---------------------------------------------------------------------
+        private sealed class EnumerableIterator : DataPointIterator
+        {
+            private readonly IEnumerator<DataPoint> _enumerator;
+            //-----------------------------------------------------------------
+            public EnumerableIterator(IEnumerator<DataPoint> enumerator) => _enumerator = enumerator;
+            //-----------------------------------------------------------------
+            public override DataPointIterator Clone() => new EnumerableIterator(_enumerator);
+            //-----------------------------------------------------------------
+            public override bool MoveNext()
+            {
+                if (_enumerator.MoveNext())
+                {
+                    _current = _enumerator.Current;
+                    return true;
+                }
+
+                return false;
+            }
+        }
     }
 }
---
+++ tests/gfoidl.DataCompression.Tests/Compression/NoCompressionTests/ProcessCore.cs
@@ -0,0 +1,159 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using NUnit.Framework;
+
+namespace gfoidl.DataCompression.Tests.Compression.NoCompressionTests
+{
+    [TestFixture]
+    public class ProcessCore
+    {
+        private static readonly DataPointSerializer _ser = new DataPointSerializer();
+        //---------------------------------------------------------------------
+        [Test]
+        public void Data_given_as_IEnumerable___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForTrend();
+            var expected = RawDataForTrend().ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Data_given_as_List___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForTrend().ToList();
+            var expected = RawDataForTrend().ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Data_given_as_IList___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForTrend().ToList().AsReadOnly();
+            var expected = RawDataForTrend().ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Data_IEnumerable_with_maxDeltaX___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForMaxDelta();
+            var expected = RawDataForMaxDelta().ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Data_List_with_maxDeltaX___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForMaxDelta().ToList();
+            var expected = RawDataForMaxDelta().ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Data_IList_with_maxDeltaX___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForMaxDelta().ToList().AsReadOnly();
+            var expected = RawDataForMaxDelta().ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void One_DataPoint_IEnumerable___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForMaxDelta().Take(1);
+            var expected = RawDataForMaxDelta().Take(1).ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void One_DataPoint_List___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForMaxDelta().Take(1).ToList();
+            var expected = RawDataForMaxDelta().Take(1).ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void One_DataPoint_IList___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForMaxDelta().Take(1).ToList().AsReadOnly();
+            var expected = RawDataForMaxDelta().Take(1).ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Two_DataPoint_IEnumerable___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForMaxDelta().Take(2);
+            var expected = RawDataForMaxDelta().Take(2).ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Two_DataPoint_List___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForMaxDelta().Take(2).ToList();
+            var expected = RawDataForMaxDelta().Take(2).ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        [Test]
+        public void Two_DataPoint_IList___OK()
+        {
+            var sut      = new NoCompression();
+            var data     = RawDataForMaxDelta().Take(2).ToList().AsReadOnly();
+            var expected = RawDataForMaxDelta().Take(2).ToList();
+
+            var actual = sut.Process(data).ToList();
+
+            CollectionAssert.AreEqual(expected, actual);
+        }
+        //---------------------------------------------------------------------
+        private static IEnumerable<DataPoint> RawDataForTrend()    => _ser.Read("../../../../../doc/data/dead-band/trend_raw.csv");
+        private static IEnumerable<DataPoint> RawDataForMaxDelta() => _ser.Read("../../../../../doc/data/dead-band/maxDelta_raw.csv");
+    }
+}
