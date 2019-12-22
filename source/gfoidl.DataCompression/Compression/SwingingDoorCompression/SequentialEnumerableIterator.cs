11e557631b20968977e16d1592b8d3fce9bd7391
Günther Foidl <gue@korporal.at>
2019-12-22 20:39:09 +0100

    Fixed bug in Clone of Iterator (#52)
    * Tests
    
    * Fix

--- source/gfoidl.DataCompression/Compression/SwingingDoorCompression/SequentialEnumerableIterator.cs
+++ source/gfoidl.DataCompression/Compression/SwingingDoorCompression/SequentialEnumerableIterator.cs
@@ -7,17 +7,14 @@ namespace gfoidl.DataCompression.Internal.SwingingDoor
 {
     internal sealed class SequentialEnumerableIterator : SwingingDoorCompressionIterator
     {
-        public SequentialEnumerableIterator(
-            SwingingDoorCompression swingingDoorCompression,
-            IEnumerable<DataPoint>? source,
-            IEnumerator<DataPoint>? enumerator = null)
+        public SequentialEnumerableIterator(SwingingDoorCompression swingingDoorCompression, IEnumerable<DataPoint>? source)
             : base(swingingDoorCompression)
         {
-            _source     = source     ?? throw new ArgumentNullException(nameof(source));
-            _enumerator = enumerator ?? source.GetEnumerator();
+            _source     = source ?? throw new ArgumentNullException(nameof(source));
+            _enumerator = source.GetEnumerator();
         }
         //---------------------------------------------------------------------
-        public override DataPointIterator Clone() => new SequentialEnumerableIterator(_swingingDoorCompression, _source, _enumerator);
+        public override DataPointIterator Clone() => new SequentialEnumerableIterator(_swingingDoorCompression, _source);
         //---------------------------------------------------------------------
 #if NETSTANDARD2_1
         public override ValueTask<DataPoint[]> ToArrayAsync(CancellationToken ct)    => throw new NotSupportedException();
---
+++ tests/gfoidl.DataCompression.Tests/Compression/NoCompressionTests/Clone.cs
@@ -0,0 +1,37 @@
+﻿using System.Linq;
+using NUnit.Framework;
+
+namespace gfoidl.DataCompression.Tests.Compression.NoCompressionTests
+{
+    public class Clone : Base
+    {
+        [Test]
+        public void Cloning_iterates_over_fresh_set()
+        {
+            var sut      = new NoCompression();
+            var data = KnownSequence().ToArray().Select(dp => dp);
+            var expected = KnownSequence().ToArray();
+            var filter   = sut.Process(data);
+
+            var iterator = filter.GetEnumerator();
+            iterator.MoveNext();
+
+            iterator = filter.Clone().GetEnumerator();
+
+            Assert.Multiple(() =>
+            {
+                int step = 0;
+                Assert.IsTrue(iterator.MoveNext(), $"MoveNext step: {step}");
+                Assert.AreEqual(expected[step], iterator.Current, $"Equal step: {step}");
+                step++;
+                Assert.IsTrue(iterator.MoveNext(), $"MoveNext step: {step}");
+                Assert.AreEqual(expected[step], iterator.Current, $"Equal step: {step}");
+                step++;
+                Assert.IsTrue(iterator.MoveNext(), $"MoveNext step: {step}");
+                Assert.AreEqual(expected[step], iterator.Current, $"Equal step: {step}");
+                step++;
+                Assert.IsFalse(iterator.MoveNext(), $"MoveNext step: {step++}");
+            });
+        }
+    }
+}
