a76aea196d452577a602e9dda51dc204c465001e
Günther Foidl <gue@korporal.at>
2020-12-11 16:26:46 +0100

    Fixed data path

--- tests/gfoidl.DataCompression.Tests/Compression/DeadBandCompressionTests/Base.cs
+++ tests/gfoidl.DataCompression.Tests/Compression/DeadBandCompressionTests/Base.cs
@@ -1,4 +1,4 @@
-﻿// (c) gfoidl, all rights reserved
+// (c) gfoidl, all rights reserved
 
 using System.Collections.Generic;
 
@@ -14,10 +14,10 @@ namespace gfoidl.DataCompression.Tests.Compression.DeadBandCompressionTests
     {
         private static readonly DataPointSerializer s_ser = new DataPointSerializer();
         //---------------------------------------------------------------------
-        protected static IEnumerable<DataPoint> RawDataForTrend()     => s_ser.Read("../../../../../doc/data/dead-band/trend_raw.csv");
-        protected static IEnumerable<DataPoint> ExpectedForTrend()    => s_ser.Read("../../../../../doc/data/dead-band/trend_compressed.csv");
-        protected static IEnumerable<DataPoint> RawDataForMaxDelta()  => s_ser.Read("../../../../../doc/data/dead-band/maxDelta_raw.csv");
-        protected static IEnumerable<DataPoint> ExpectedForMaxDelta() => s_ser.Read("../../../../../doc/data/dead-band/maxDelta_compressed.csv");
+        protected static IEnumerable<DataPoint> RawDataForTrend()     => s_ser.Read("../../../../../data/dead-band/trend_raw.csv");
+        protected static IEnumerable<DataPoint> ExpectedForTrend()    => s_ser.Read("../../../../../data/dead-band/trend_compressed.csv");
+        protected static IEnumerable<DataPoint> RawDataForMaxDelta()  => s_ser.Read("../../../../../data/dead-band/maxDelta_raw.csv");
+        protected static IEnumerable<DataPoint> ExpectedForMaxDelta() => s_ser.Read("../../../../../data/dead-band/maxDelta_compressed.csv");
         //---------------------------------------------------------------------
 #if NETCOREAPP
         protected static async IAsyncEnumerable<DataPoint> RawDataForTrendAsync([EnumeratorCancellation] CancellationToken ct = default)
